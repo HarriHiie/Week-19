aa7e3188d30882464c12c8b394c170a672c77f81
HarriHiie <89829583+HarriHiie@users.noreply.github.com>
2022-01-26 15:12:39 +0200

    Add files via upload

---
+++ Week-19/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SnakeOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+            int x = 300;
+            //drawing a game field frame
+            Walls walls = new Walls(40, 25);
+            walls.Draw();
+
+            Point snakeTail = new Point(15, 15, '◄');
+            Snake snake = new Snake(snakeTail, 5, Direction.RIGHT);
+            snake.Draw();
+
+            FoodGenerator foodGenerator = new FoodGenerator(80, 25, '¤');
+            Point food = foodGenerator.GenerateFood();
+            food.Draw();
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            while (true)
+            {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+
+
+                if (snake.Eat(food))
+                {
+                    food = foodGenerator.GenerateFood();
+                    food.Draw();
+                    score++;
+                    x = x - 5;
+                    Console.Beep();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKeys(key.Key);
+                }
+                Thread.Sleep(x);
+
+
+
+            }
+            string str_score = Convert.ToString(score);
+            WriteGameOver(str_score);
+            Console.ReadLine();
+        }
+
+        public static void WriteGameOver(string score)
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("////////////////////", xOffset, yOffset++);
+            WriteText("       Mäng Läbi       ", xOffset + 1, yOffset++);
+            yOffset++;
+            WriteText($" Sa Said {score} Punkti", xOffset + 2, yOffset++);
+            WriteText("", xOffset + 1, yOffset++);
+            WriteText("////////////////////", xOffset, yOffset++);
+        }
+
+
+        public static void WriteText(string text, int xOffset, int YOffset)
+        {
+            Console.SetCursorPosition(xOffset, YOffset);
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Week-19/Walls.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeOOP
+{
+    class Walls
+    {
+        List<Figure> wallList;
+
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallList = new List<Figure>();
+            Horizon top = new Horizon(0, 80, 0, 'S');
+            VerticalLine left = new VerticalLine(0, 25, 0, 'S');
+            Horizon bottom = new Horizon(0, 80, 25, 'S');
+            VerticalLine right = new VerticalLine(0, 25, 80, 'S');
+
+
+            VerticalLine obstacle = new VerticalLine(10, 20, 50, 'S');
+            Horizon obstacle1 = new Horizon(7, 40, 7, 'S');
+            
+
+
+            wallList.Add(top);
+            wallList.Add(left);
+            wallList.Add(bottom);
+            wallList.Add(right);
+            wallList.Add(obstacle);
+            wallList.Add(obstacle1);
+            
+        }
+        public void Draw()
+        {
+            foreach (var wall in wallList)
+            {
+                wall.Draw();
+            }
+        }
+        public bool IsHit(Figure figure)
+        {
+            foreach (var wall in wallList)
+            {
+                if (wall.IsHit(figure))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
